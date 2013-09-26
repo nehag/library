954feacf182406cfedfc1d0ace44483744093354
Ramya G <ramyag77@gmail.com>
2013-09-26 01:31:38 -0600

    Neha commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    # This is next commit
     @user = User.find(params[:id])
 
     respond_to do |format|
